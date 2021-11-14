Fix: correct fn & fp calculation & cast x,y to float between computing the difference bewteen label and ground truth to display final output correctly

student/objdet_eval.py
@@ -54,16 +54,16 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
             for obj in detections:
                 ## step 3 : extract the four corners of the current detection
                 _, x_o, y_o, z_o, h_l, w_o, l_o, yaw_o  = obj
-                obj_label = tools.compute_box_corners(x_o, y_o, w_o, l_o, yaw_o ) 
+                obj_label = tools.compute_box_corners(x_o, y_o, w_o, l_o, yaw_o) 
                 ## step 4 : computer the center distance between label and detection bounding-box in x, y, and z
-                dist_x = abs(x_l - x_o)
-                dist_y = abs(y_l - y_o)
-                dist_z = abs(z_l - z_o)
+                dist_x = np.float_(x_l) - np.float_(x_o)
+                dist_y = np.float_(y_l) - np.float_(y_o)
+                dist_z = np.float_(z_l) - np.float_(z_o)
                 ## step 5 : compute the intersection over union (IOU) between label and detection bounding-box
                 a = Polygon(corner_label)
                 b = Polygon(obj_label)
                 iou =  a.intersection(b).area / a.union(b).area
-                ## step 6 : if IOU exceeds min_iou threshold, store [iou,dist_x, dist_y, dist_z] in matches_lab_det and increase the TP count
+                ## step 6 : if IOU exceeds min_iou threshold, store [iou, dist_x, dist_y, dist_z] in matches_lab_det and increase the TP count
                 if(iou > min_iou):
                     matches_lab_det.append([iou, dist_x, dist_y, dist_z])
                     true_positives = true_positives + 1
@@ -86,10 +86,10 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
     all_positives = np.count_nonzero(labels_valid)
 
     ## step 2 : compute the number of false negatives
-    false_negatives = max(len(detections) - len(ious), 0)
+    false_negatives = all_positives - true_positives
 
     ## step 3 : compute the number of false positives
-    false_positives = max(all_positives - len(ious), 0)
+    false_positives = len(detections) - true_positives
     
     #######
     ####### ID_S4_EX2 END #######     